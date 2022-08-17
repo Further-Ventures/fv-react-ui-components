b1e031958858725384dd583726e7dcb966c55389
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-08-17 11:41:53 +0300

    Npm install tab - added Tooltip

--- src/storybook/addons/NpmInstall/CodeBox/index.tsx
+++ src/storybook/addons/NpmInstall/CodeBox/index.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import CopyIcon from '../CopyIcon';
 import useStyles from './styles';
+import Tooltip from '@stryberventures/gaia-react.tooltip';
 
 interface ICodeBoxProps {
   children: string[];
 }
 
 const CodeBox = ({ children }: ICodeBoxProps) => {
+  const [copyString, setCopyString] = React.useState('Press to Copy');
   const classes = useStyles();
-
+  
   return (
     <div className={classes.codebox}>
       <pre className={classes.children}>{children}</pre>
-      <CopyToClipboard text={children.join('')}>
-        <div className={classes.copyContainer}>
-          <CopyIcon fill="#000" />
-        </div>
-      </CopyToClipboard>
+      <Tooltip title={copyString}>
+        <CopyToClipboard
+          text={children.join('')}
+          onCopy={() => setCopyString('Copied!')}
+        >
+          <div
+            className={classes.copyContainer}
+            onMouseLeave={() => setCopyString('Press to Copy')}
+          >
+            <CopyIcon fill="#000" />
+          </div>
+        </CopyToClipboard>
+      </Tooltip>
     </div>
   );
 };
