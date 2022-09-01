272a4960dd2f74ad18d2ce9a9a27039ae6e9603a
dtomaszewski88 <53857609+dtomaszewski88@users.noreply.github.com>
2022-09-01 15:37:18 +0200

    Feature/hg 47 button (#10)
    * add button component
    
    * add tests
    
    * change package name
    
    * fix naming
    
    * minor changes
    
    * lint fix
    
    * remove obsolete test
    
    * minor fix

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ['<rootDir>/build/'],
+  moduleDirectories: ['node_modules'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
