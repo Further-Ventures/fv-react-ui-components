014f7a309f163458f41f18e8b820d471456ef740
Alex Pilica <alexandru.pilica@further.ae>
2022-09-07 14:40:24 +0300

    HG-65

---
+++ src/components/Elevation/Elevation.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Elevation } from './index';
+import pkg from './package.json';
+import { buildExcludeArgTypes } from '../../storybook/utils';
+
+export default {
+  title: 'Components/Elevation',
+  component: Elevation,
+  parameters: {
+    pkg
+  },
+  argTypes: buildExcludeArgTypes(['className'])
+} as ComponentMeta<typeof Elevation>;
+
+const Template: ComponentStory<typeof Elevation> = (args) => {
+  return <Elevation {...args} className='w-40 h-40 m-10 flex items-center justify-center rounded-md'>
+    some text
+  </Elevation>;
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  variation: 'extraLight'
+};
---
+++ src/components/Elevation/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+
+interface IElevationProps {
+  variation?: 'extraLight' | 'light' | 'medium' | 'heavy' | 'extraHeavy'
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Elevation: React.FC<IElevationProps> = ({
+  variation = 'medium',
+  className,
+  children
+}) => {
+
+  return <div
+    className={classNames(`shadow-${variation}`, className)}
+  >
+    {children}
+  </div>
+};
+
+Elevation.defaultProps = {
+  variation: 'medium'
+};
